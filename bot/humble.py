a133952984dc1aa6e3ce28f27cb24773c8b5df76
cakedev <calemaynes@gmail.com>
2021-06-04 20:46:03 -0600

    changed to using discord.Cog classes

--- bot/humble.py
+++ bot/humble.py
@@ -1,15 +1,11 @@
-from player import Player
+import os
 import discord
 from discord.ext import commands
-import os
-import json
-import datetime
+
 
 # change other's nickname for an hour
 # bencoin reacts give sender a coin
 # .imtrapped posts a pic of a trap
-
-bot = commands.Bot(command_prefix=".")
 
 try:
     TOKEN = os.environ["DISCORD_TOKEN"]
@@ -19,59 +15,94 @@ except:
     raise discord.DiscordException
 
 
-@bot.event
-async def on_ready():
+class Bot(commands.cog):
 
-    print(GUILD)
-    print(bot.guilds)
-    # guilds = discord.utils.find(lambda g:  g.name == GUILD, bot.guilds)
+    def __init__(self, command_prefix, self_bot):
+        commands.Bot.__init__(
+            self, command_prefix=command_prefix, self_bot=self_bot)
 
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Game("Listening to .help"))
+        self.added_commands()
 
-    print(
-        f'{bot.user.name} is connected to the following guild:\n'
-        # f'{guild.name} (id: {guild.id})'
-    )
+    def added_commands(self):
+        @self.event
+        async def on_ready():
+            # guilds = discord.utils.find(lambda g:  g.name == GUILD, bot.guilds)
 
+            await bot.change_presence(status=discord.Status.online, activity=discord.Game("Listening to .help"))
 
-@bot.command(help="PONG!")
-async def ping(ctx):
-    await ctx.send(f"🏓 Pong with {str(round(bot.latency, 2))}")
+            print(f'{bot.user.name} is connected\n')
 
+        @self.event
+        async def on_command_error(ctx, error):
+            if isinstance(error, commands.errors.CheckFailure):
+                await ctx.send('You do not have the correct role for this command.')
 
-@bot.command(help="Who sent this message?")
-async def whoami(ctx):
-    await ctx.send(f"You are {ctx.message.author.name}")
+        @self.command(help="PONG!")
+        async def ping(ctx):
+            await ctx.send(f"🏓 Pong with {str(round(bot.latency, 2))}")
 
+        @self.command(help="Who sent this message?")
+        async def whoami(ctx):
+            await ctx.send(f"You are {ctx.message.author.name}")
 
-@bot.command(help="clears the last 2 messages. FOR TEST PURPOSES")
-async def clear(ctx, amount=3):
-    await ctx.channel.purge(limit=amount)
+        @self.command(help="clears the last 2 messages. FOR TEST PURPOSES")
+        async def clear(ctx, amount=3):
+            await ctx.channel.purge(limit=amount)
 
+        @self.command(help="adds two numbers separated by spaces")
+        async def add(ctx, a: int, b: int):
+            await ctx.send(a + b)
 
-@bot.command(help="adds two numbers separated by spaces")
-async def add(ctx, a: int, b: int):
-    await ctx.send(a + b)
-
-
-@bot.command()
-@commands.has_role("Archduke of Celibacy")
-async def init(ctx):
-    data = {
-        "id": ctx.message.author,
-        "name": ctx.message.author.name,
-        "accounts": []
-    }
-    print(data)
-    user = Player(data)
-    data["accounts"].append(user.add_account())
-    print(data)
-
-
-@ bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('You do not have the correct role for this command.')
+        @self.command()
+        @commands.has_role("Archduke of Celibacy")
+        async def init(ctx):
+            data = {
+                "id": ctx.message.author.id,
+                "name": ctx.message.author.name + '#' + ctx.message.author.discriminator,
+                "accounts": []
+            }
+            print(data)
+            user = Player(data)
+            print(user)
+            user.add_account()
+            print(user)
 
 
+bot = Bot(command_prefix=".", self_bot=False)
 bot.run(TOKEN)
+
+
+class Player(commands.Cog):
+    id: str
+    name: str
+
+    balance: int
+
+    def __str__(self) -> str:
+        return 'id= {} name= {} balance= {}'.format(self.id, self.name, self.balance)
+
+    def __init__(self, bot) -> None:
+
+        return self
+        # def __init__(self, data):
+
+        #     self.update(data)
+
+        # def update(self, data):
+        #     self.id = data['id']
+        #     self.name = data['name']
+        #     self.balance = data['balance']
+
+    async def set_balance(self, amount):
+        self.balance = amount
+        await self.balance
+
+    async def deposit(self, amount):
+        await self.balance + amount
+
+    async def withdraw(self, amount):
+        if self.balance > amount:
+            await self.balance - amount
+        else:
+            print(f"account balance too low! account: {self.name}")
+            return ValueError
--- bot/player.py
+++ bot/player.py
@@ -1,32 +1,23 @@
-class Player:
+class Player(commands.Cog):
     id: str
     name: str
 
-    accounts: list
-
-    def __init__(self, data):
-        self.update(data)
-
-    def update(self, data):
-        self.id = data['id']
-        self.name = data['name']
-        self.accounts = data['accounts']
-
-    def add_account(self, account):
-        self.accounts.append(account)
-        return self.accounts
-
-
-class Account:
-    id: str
     balance: int
 
-    def __init__(self, data):
-        self.update(data)
+    def __str__(self) -> str:
+        return 'id= {} name= {} balance= {}'.format(self.id, self.name, self.balance)
 
-    def update(self, data):
-        self.id = data['id']
-        self.balance = data['balance']
+    def __init__(self, bot) -> None:
+
+        return self
+        # def __init__(self, data):
+
+        #     self.update(data)
+
+        # def update(self, data):
+        #     self.id = data['id']
+        #     self.name = data['name']
+        #     self.balance = data['balance']
 
     def set_balance(self, amount):
         self.balance = amount
